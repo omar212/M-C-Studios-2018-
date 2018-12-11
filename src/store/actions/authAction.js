adca3c86b76849107d45861105fea8eb905917aa
Yufeng Liu <yliu5683@gmail.com>
2018-12-11 17:01:27 -0500

    updated authAction

--- src/store/actions/authAction.js
+++ src/store/actions/authAction.js
@@ -31,16 +31,18 @@ export const signUp = (newUser) => {
     firebase.auth().createUserWithEmailAndPassword(
       newUser.email,
       newUser.password
-    ).then(resp => {
-      return firestore.collection('users').doc(resp.user.uid).set({
+    ).then((response) => {
+      return firestore.collection('users').doc(response.user.uid).set({
         firstName: newUser.firstName,
         lastName: newUser.lastName,
-        initials: newUser.firstName[0] + newUser.lastName[0]
-      });
+        initials: newUser.firstName[0] + newUser.lastName[0],
+        cart: [],
+        wishList: []
+      })
     }).then(() => {
-      dispatch({ type: 'SIGNUP_SUCCESS' });
-    }).catch((err) => {
-      dispatch({ type: 'SIGNUP_ERROR', err});
-    });
+      dispatch({ type: 'SIGNUP_SUCCESS'})
+    }).catch(error => {
+      dispatch({ type: 'SIGNUP_ERROR', error})
+    })
   }
 }
