dbd6432f2d09c406c18c55b268e5e2a2340c45c4
Yufeng Liu <yliu5683@gmail.com>
2018-12-11 18:52:15 -0500

    updated bar

--- src/components/elements/Bar/Bar.js
+++ src/components/elements/Bar/Bar.js
@@ -1,29 +1,29 @@
-// import React, { Component } from 'react';
-// import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
-// import { Link } from 'react-router-dom';
-// import Main from '../../App/App';
-// import './Bar.css';
-//
-//
-// class Bar extends Component  {
-//   render(){
-//     return(
-//       <div style={{height: '50px', position: 'relative'}}>
-//         <Layout fixedHeader>
-//           <Header style={{backgroundColor: 'black'}} title={<span style={{ fontSize: '30px' , fontFamily: 'Petit Formal Script, cursive'}}>Just Chill</span>}>
-//              <Navigation>
-//                 <Link to="/">Home Page</Link>
-//                 <Link to="/Login">Login</Link>
-//                 <Link to="/WishList">Wish List</Link>
-//                 <Link to="/Cart">Cart</Link>
-//             </Navigation>
-//           </Header>
-//         <Content>
-//         </Content>
-//       </Layout>
-//     </div>
-//     );
-//   }
-// }
-//
-// export default Bar;
+import React from 'react';
+import { Link } from 'react-router-dom';
+import SignedIn from '../../NavigationBar/Login/SignedIn';
+import SignedOut from '../../NavigationBar/Login/SignedOut';
+import { connect } from 'react-redux';
+
+
+const Bar = (props) => {
+    const { auth } = props
+    console.log(auth)
+    const links =  auth.uid ? <SignedIn /> : <SignedOut />
+    return(
+      <nav className="nav-wrapper black darken-3">
+        <div className="container">
+          <Link to='/' className="brand-logo">M-C Studios</Link>
+          { links }
+        </div>
+      </nav>
+    );
+
+}
+
+const mapStateToProps = (state) => {
+  return {
+    auth: state.firebase.auth
+  }
+}
+
+export default connect(mapStateToProps)(Bar);
