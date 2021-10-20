1062757db417a3c1d3a9236318bd69690585aef8
omar212 <omarelnagdy16@gmail.com>
2021-10-20 15:25:49 -0400

    Updates

---
+++ precache-manifest.ce650f3222ba90c258cc0886dbbde28b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e2d6fb9c52106518fa65",
+    "url": "/M-C-Studios-2018-/static/css/main.b9b347af.chunk.css"
+  },
+  {
+    "revision": "e2d6fb9c52106518fa65",
+    "url": "/M-C-Studios-2018-/static/js/main.e2d6fb9c.chunk.js"
+  },
+  {
+    "revision": "074275f7501efb80cf79",
+    "url": "/M-C-Studios-2018-/static/css/1.e4f571b0.chunk.css"
+  },
+  {
+    "revision": "074275f7501efb80cf79",
+    "url": "/M-C-Studios-2018-/static/js/1.074275f7.chunk.js"
+  },
+  {
+    "revision": "b5e8be6558b446fdb300",
+    "url": "/M-C-Studios-2018-/static/js/runtime~main.b5e8be65.js"
+  },
+  {
+    "revision": "3bb7949045cb18603cb975787511dccd",
+    "url": "/M-C-Studios-2018-/index.html"
+  }
+];
