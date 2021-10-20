ce85c397f1e89d24f8d64d4e9b277f039166e992
omar212 <omarelnagdy16@gmail.com>
2021-10-20 16:01:31 -0400

    fix

--- src/configFire/fire.js
+++ src/configFire/fire.js
@@ -2,14 +2,16 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 
 // Initialize Firebase
-var config = {
-  apiKey: "AIzaSyDY_ZQTHd4bNNNOZ1lTTOSvUzo0-XuSVJ8",
-    authDomain: "offical-movie-website.firebaseapp.com",
-    databaseURL: "https://offical-movie-website.firebaseio.com",
-    projectId: "offical-movie-website",
-    storageBucket: "offical-movie-website.appspot.com",
-    messagingSenderId: "599088107743"
+const config = {
+  apiKey: "AIzaSyB5viw5l3mYUkgbRHjlhyi9aUHgfmwpFz0",
+  authDomain: "new-movie-website-5967f.firebaseapp.com",
+  projectId: "new-movie-website-5967f",
+  storageBucket: "new-movie-website-5967f.appspot.com",
+  messagingSenderId: "708802755202",
+  appId: "1:708802755202:web:7f2ee4634c19f1d3ced29b",
+  measurementId: "G-8SZJMKLM5B"
 };
+
 
 firebase.initializeApp(config);
 
